3ba724aaedae3ae5ce9057e1727c21804331ee3b
Pradeep Kumar <PKumar1@mindtree_odc01.com>
2018-08-05 22:33:22 +0530

    Initial commit

---
+++ helloWorld/src/main/java/com/mindtree/jenkinsTest/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.mindtree.jenkinsTest;
+
+/**
+ * @author PKumar1
+ *
+ */
+public class HelloWorld {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//this is just for testing
+		//this is just for testing
+		System.out.println("Hello World!!!");
+	}
+
+}

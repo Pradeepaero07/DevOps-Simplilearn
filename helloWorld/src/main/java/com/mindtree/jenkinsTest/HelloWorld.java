7b3907d5584df02ed9769d48c97092a59d18bcef
Pradeepaero07 <pradeepaero07@gmail.com>
2018-08-07 15:22:42 +0530

    Removed unnecessary comments

--- helloWorld/src/main/java/com/mindtree/jenkinsTest/HelloWorld.java
+++ helloWorld/src/main/java/com/mindtree/jenkinsTest/HelloWorld.java
@@ -4,27 +4,11 @@ package com.mindtree.jenkinsTest;
  * @author PKumar1
  *
  */
-
-//this is just for testing
-		//this is just for testing
-		//this is just for testing
-//this is just for testing
-		//this is just for testing
-		//this is just for testing
 public class HelloWorld {
-	
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//this is just for testing
-		//this is just for testing
-		//this is just for testing
-		//this is just for testing
-		//this is just for testing
-		//this is just for testing
-		//this is just for testing
 		System.out.println("Hello World!!!");
 	}
 
